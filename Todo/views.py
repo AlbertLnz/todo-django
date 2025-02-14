6d924fcca0c3469d79a88da3511de47060cc04cd
Albert Lanza <albert2000_lanza@hotmail.com>
2025-02-14 10:37:13 +0100

    Fix errors & do read tasks, delete task & create task

--- Todo/views.py
+++ Todo/views.py
@@ -1,32 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Task
+from django.shortcuts import render, redirect, get_object_or_404
+# from django.contrib.auth.models import User
+from .models import Task, Category, Author
+from .forms import TaskForm
 
 # Create your views here.
 
-def first_fnct(request):
-    # return HttpResponse({'message': 'Hello World!'})
-    return render(request, 'index.html')
+def show_index_page(request):
+    return render(request, 'index.html', { 'title': "Aplicació To-Do"})
+
 
 def list_tasks(request):
+    # Aqui utilizo 'Author' en compte de 'User' perque son molt semblants (es diferencien només que User no té dni)
+    # I en la vista, només vull el "name", així que es indiferent
+    author = Author.objects.all()
     tasks = Task.objects.all()
-    return render(request, 'Todo/list_tasks.html', {'tasks':
-    tasks})
+    categories = Category.objects.all()
+
+    return render(request, 'list_tasks.html', {'authors': author, 'tasks': tasks, 'categories': categories})
 
 def add_task(request):
-    # if request.method == 'POST':
-    # form = LibroForm(request.POST)
-    # if form.is_valid():
-    # form.save()
-    # return redirect('lista_libros')
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
 
-    # else:
-    # form = LibroForm()
-    # return render(request, 'libros/form_libro.html', {'form': form})
+        if form.is_valid():
+            title = request.POST.get('title')
+            author_id = request.POST.get('author_id')
+            categories_ids = request.POST.getlist('categories_ids')
 
-    pass
+            author = Author.objects.get(id=author_id)
+            categories = Category.objects.filter(id__in=categories_ids)
 
-def editar_libro(request, id):
+            task = Task.objects.create(
+                title=title,
+                author=author,
+                completed=False,
+            )
+            task.categories.set(categories) # En les relacion Many-To-Many, s'ha d'afegir amb un 'set'
+
+            task.save()
+            return redirect('llistar_tasques')
+        else:
+            return redirect('index')
+
+    else:
+        return TaskForm()
+
+def edit_task(request, id):
     # libro = get_object_or_404(Libro, id=id)
     # if request.method == 'POST':
     # form = LibroForm(request.POST, instance=libro)
@@ -38,3 +57,16 @@ def editar_libro(request, id):
     # form = LibroForm(instance=libro)
     # return render(request, 'libros/form_libro.html', {'form': form})
     pass
+
+def delete_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        task.delete()
+    return redirect('llistar_tasques')
+
+# def eliminar_libro(request, id):
+#     libro = get_object_or_404(Libro, id=id)
+#     if request.method == 'POST':
+#         libro.delete()
+#         return redirect('lista_libros')
+#     return render(request, 'aplicacion1/eliminar_libro.html', {'libro': libro})
---
+++ todo.md
@@ -0,0 +1,2 @@
+[] Fer la vista + template de UPDATE
+[] Fer el CRUD de User + Author
