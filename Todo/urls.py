90b5f30608bc410dd4d4818cf7f55259600b44e0
mar-falomi <marhk35@gmail.com>
2025-02-14 19:30:32 +0100

    Update fnct & edit view

--- Todo/urls.py
+++ Todo/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', show_index_page, name='index'),
     path('list/', list_tasks, name='llistar_tasques'),
     path('agregar/', add_task, name='afegir_task'),
-    path('editar/<int:id>/', edit_task),
+    path('editar/<int:task_id>/', edit_task, name= 'editar_task'),
     path('eliminar/<int:id>/', delete_task, name='eliminar_task'),
 ]
